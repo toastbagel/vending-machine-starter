786bc2ebf8c0cf8169548403377472f103bf9d4d
Glitch (codenation-vendingmachine-ko) <none>
2021-02-09 22:55:42 +0000

    🌷📕 Updated with Glitch

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var money = 10.00;
-var cokes = 0;
-var chips = 0;
+var cokes = 1.50;
+var chips = 1.25;
 
 $(".buy").click(function() {
+  let item = $("#item").val();
+  if(item === "") {
+    $(".message").text("you must enter an item");
+  } else if (item === "coke") {
+    money = money - cokes;
+    $("#moneyleft").html(money);
+  } else if (item === "chips") {
+    money = money - chips;
+    $("#moneyleft").html(money);
+  }
 });
